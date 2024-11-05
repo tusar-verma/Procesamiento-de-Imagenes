71a232b3e7d81f1c1ba8dabb650c71c4b3745b98
Victor <victorolivarez2001@hotmail.com>
2024-11-05 10:20:16 -0300

    pulido y correcciones

--- Labo-6/taller_Hough.py
+++ Labo-6/taller_Hough.py
@@ -5,15 +5,6 @@ from skimage.draw import line
 from scipy import ndimage
 
 def BuildImage():
-    image_line = np.zeros((50,50))
-
-    rr,cc = line(20,20,40,40)
-
-    image_line[rr,cc] = 255
-
-    rr,cc = line(10,45,40,45)
-
-    image_line[rr,cc] = 255
 
     image_point2 = np.zeros((50,50))
 
@@ -35,115 +26,95 @@ def BuildImage():
 
     image_point5 = np.zeros((50,50))
 
-    image_point5[15,20] = 255
+    image_point5[25,20] = 255
     image_point5[45,45] = 255
     image_point5[1,20] = 255
-    image_point5[12,20] = 255
+    image_point5[48,20] = 255
+    image_point5[34,38] = 255
 
 
-    return image_line , image_point2, image_point3, image_point4, image_point5
+    return  image_point2, image_point3, image_point4, image_point5
 
-def filtroMaxLocal(A, tam=3):
-    M1 = A.shape[0]
-    N1 = A.shape[1]
-    diff = tam//2
-    li = np.arange(-diff,diff+1)
-    lj = np.arange(-diff,diff+1)
-    C = np.zeros((M1, N1))
-    maximoTotal = np.max(A)
-    for u in range(C.shape[0]):
-        for v in range(C.shape[1]):
-            valores = []
-            for i in li:
-                for j in lj:
-                    ai = u - i
-                    aj = v - j
-                    
-                    if(0<=ai<M1 and 0<=aj<N1):
-                        #if(u==0 and v==44):
-                        #   print(ai,aj,A[ai,aj],i!=0,j!=0, u,v)
-                        if(u!=ai or v!=aj):
-                            valores.append(A[ai][aj])
-            valores = np.array(valores)
-            C[u,v] = np.prod(A[u,v] > valores) and A[u,v]>1 
-            #if(u==0 and v==44 and C[u,v]==1):
-                #print(A[u,v],A[u,v+1],valores)
-            
-    return C
-
-
-def trasnformdaHough(imagen, limsuptita=90, limphi=50):
-    titas = np.arange(-limsuptita,limsuptita)
-    phies = np.arange(-limphi,limphi)
-    #calculo de acumulada
+def trasnformdaHough(imagen):
+    #Se recorren todas las posibles rectas entre phi=-sqrt(2)*50 a phi=sqrt(2)*50 y entre tita=-90° hasta tita=90°
+    limphi = np.round(50*np.sqrt(2))
+    limtita = 90
+    titas = np.arange(-limtita,limtita,1)
+    phies = np.arange(-limphi,limphi,1)
+    #Calculo de acumulada
     acumulada =  np.zeros((phies.size,titas.size))
     for x in range(imagen.shape[0]):
         for y in range(imagen.shape[1]):
             if (imagen[x,y] == 255):
-                for tita in titas:
+                for i in range(titas.size):
+                    tita = titas[i]
+                    #Se transforma a radianes
                     titarad = tita*np.pi/180
-                    titanorm = tita + 90
-                    phi = int(x*np.cos(titarad) + y*np.sin(titarad))
+                    #Se calcula el valor de phi para ese angulo redondeando a la unidad
+                    phi = np.round(x*np.cos(titarad) + y*np.sin(titarad))
+                    #Buscamos el indice de ese valor de phi en la lista de phies que se toman en cuenta
+                    phinorm = np.where(phies == phi)
                     if(phi in phies):
-                        phinorm = phi + limphi
-                        acumulada[phinorm,titanorm] += 1
-    #maximosLocales = filtroMaxLocal(acumulada,7)
-    #print(np.max(acumulada))
-    acumulada_filtrada = np.max(acumulada)==acumulada
-    #representar las lineas en la imagen
+                        #Si el phi está, se suma uno a la acumulada
+                        acumulada[phinorm,i] += 1
+    #Dado que se buscan lineas entre puntos, solo nos interesan las rectas con al menos 2 votos
+    acumulada_filtrada = 1<acumulada
+
+    #Aqui buscamos representar las líneas en la imagen
     imagen_lineas = np.zeros(imagen.shape)
-    for tita in titas:
-        for phi in phies:
+    for i in range(titas.size):
+        for j in range(phies.size):
+            phi = phies[j]
+            tita = titas[i]
             titarad = tita*np.pi/180
-            titanorm = tita + limsuptita
-            phinorm = phi + limphi
             m = 0
             b = phi
-            if(titarad != 0):
-                m = - np.cos(titarad)/np.sin(titarad)
-                b = phi/np.sin(titarad)
-            
-            if(acumulada_filtrada[phinorm,titanorm] == 1):
-                
-                for x in range(imagen.shape[0]):
-                    y = int(m*x + b)
-                    
-                    if(0<=y<imagen_lineas.shape[1]):
-                        
-                        imagen_lineas[x,y] = 255
-                        if(m==0):
-                            pass
+            #Si para esos valores de phi y tita la acumulada filtrada es 1 entonces la recta es relevante
+            if(acumulada_filtrada[j,i] == 1):
+                #Separamos los casos tita=0 (cuando la recta es de la forma x=cte) de los casos tita!=0 (se trata con las formulas normalmente)
+                if(titarad != 0):
+                    m = - np.cos(titarad)/np.sin(titarad)
+                    b = phi/np.sin(titarad)
+                    #Para cada valor de x en la imagen se obtiene su respectivo valor de y
+                    for x in range(imagen.shape[0]):
+                        y = int(np.round(m*x + b))
+                        #Varificamos que el valor de y calculado esté en rango
+                        if(0<=y<imagen_lineas.shape[1]):
+                            #Se saturan los puntos en la imagen correspondientes a la recta
+                            imagen_lineas[x,y] = 255
+                else:
+                    #En el caso tita = 0, se saturan los pixeles correspondientes a x=phi
+                    x = int(np.round(phi))
+                    if(0<=x<imagen.shape[0]):
+                        for y in range(imagen.shape[1]):
+                            imagen_lineas[x,y] = 255
 
     return imagen_lineas, acumulada
 
 def plot():
-    imageline, imagepoint2, imagepoint3, imagepoint4, imagepoint5 = BuildImage()
-    Houghline, acumline = trasnformdaHough(imageline)
+    #Imagenes con 2, 3, 4, 5 puntos respectivamente
+    imagepoint2, imagepoint3, imagepoint4, imagepoint5 = BuildImage()
     Houghpoint2, acumpoint2 = trasnformdaHough(imagepoint2)
     Houghpoint3, acumpoint3 = trasnformdaHough(imagepoint3)
     Houghpoint4, acumpoint4 = trasnformdaHough(imagepoint4)
     Houghpoint5, acumpoint5 = trasnformdaHough(imagepoint5)
-    fig, axes = plt.subplots(3,5, figsize = (10,10))
+    fig, axes = plt.subplots(3,4, figsize = (10,10))
     axes = axes.ravel()
-    axes[0].imshow(imageline, cmap=plt.cm.gray)
-    axes[1].imshow(imagepoint2, cmap=plt.cm.gray)
-    axes[2].imshow(imagepoint3, cmap=plt.cm.gray)
-    axes[3].imshow(imagepoint4, cmap=plt.cm.gray)
-    axes[4].imshow(imagepoint5, cmap=plt.cm.gray)
-    
-    print(Houghpoint4[35,10])
+    axes[0].imshow(imagepoint2, cmap=plt.cm.gray)
+    axes[1].imshow(imagepoint3, cmap=plt.cm.gray)
+    axes[2].imshow(imagepoint4, cmap=plt.cm.gray)
+    axes[3].imshow(imagepoint5, cmap=plt.cm.gray)
 
-    axes[5].imshow(Houghline, cmap=plt.cm.gray)
-    axes[6].imshow(Houghpoint2, cmap=plt.cm.gray)
-    axes[7].imshow(Houghpoint3, cmap=plt.cm.gray)
-    axes[8].imshow(Houghpoint4, cmap=plt.cm.gray)
-    axes[9].imshow(Houghpoint5, cmap=plt.cm.gray)
 
-    axes[10].imshow(acumline, cmap=plt.cm.gray)
-    axes[11].imshow(acumpoint2, cmap=plt.cm.gray)
-    axes[12].imshow(acumpoint3, cmap=plt.cm.gray)
-    axes[13].imshow(acumpoint4, cmap=plt.cm.gray)
-    axes[14].imshow(acumpoint5, cmap=plt.cm.gray)
+    axes[4].imshow(Houghpoint2, cmap=plt.cm.gray)
+    axes[5].imshow(Houghpoint3, cmap=plt.cm.gray)
+    axes[6].imshow(Houghpoint4, cmap=plt.cm.gray)
+    axes[7].imshow(Houghpoint5, cmap=plt.cm.gray)
+
+    axes[8].imshow(acumpoint2, cmap=plt.cm.gray)
+    axes[9].imshow(acumpoint3, cmap=plt.cm.gray)
+    axes[10].imshow(acumpoint4, cmap=plt.cm.gray)
+    axes[11].imshow(acumpoint5, cmap=plt.cm.gray)
 
 
     plt.show()
