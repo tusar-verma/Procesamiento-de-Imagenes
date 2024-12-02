c3c3dcb95563543c6314fe33eb72ad1fa4ce543f
Victor <victorolivarez2001@hotmail.com>
2024-12-02 12:50:28 -0300

    problema filtrado muy fuerte y/o calculos malos

--- TrabajoFinal/mosaic.py
+++ TrabajoFinal/mosaic.py
@@ -14,7 +14,7 @@ def getHomography(puntosP, puntosQ):
     
     #Se añade para cada punto las dos ecuaciones asociadas al sistema dado por la matriz B
     
-    for i in range(puntosP.size):
+    for i in range(puntosP.shape[0]):
         q = puntosP[i]
         p = puntosQ[i]
         f1 = [p[0], p[1], 1, 0, 0, 0, -p[0]*q[0], -p[1]*q[0], -q[0]]
@@ -28,7 +28,6 @@ def getHomography(puntosP, puntosQ):
 
     #Buscamos el indice correspondiente al menor elemento de d 
     indMin = np.argmin(d)
-
     #Este indice corresponde a la mejor solución de norma 1 del sistema
     #Así se busca el autovector de AtA=V(D^2)Vt (D^2 es abuso de notación) tal que su autovalor asociado sea lo más cercano posible a 0
     h = Vt[indMin]
@@ -41,7 +40,7 @@ def getHomography(puntosP, puntosQ):
 #Recibe matriz Homográfica H y lista de puntos p (2d)
 #Retorna pares de puntos (2d) estimados por el producto de H por p
 def productHomography(H, p):
-    puntosP3D = np.ones((3,p.size))
+    puntosP3D = np.ones((3,p.shape[0]))
     puntosP3D[:2,:] = p.T
     puntosQEstimados3D = H@puntosP3D
 
@@ -108,7 +107,7 @@ def correlacion(esquinas_p, esquinas_q, imagen_p, imagen_q):
             coef_corr = coef_corr_nom/np.sqrt(coef_corr_den_p * coef_corr_den_q)
 
             if(coef_corr > 0.8):
-                puntos_con_mayor_corr = puntos_con_mayor_corr.append([esquina_p,esquina_q])
+                puntos_con_mayor_corr.append([esquina_p,esquina_q])
     
     #puntos_con_mayor_corr = np.delete(puntos_con_mayor_corr,0,0)
     puntos_con_mayor_corr = np.array(puntos_con_mayor_corr)
@@ -117,6 +116,8 @@ def correlacion(esquinas_p, esquinas_q, imagen_p, imagen_q):
 def ransac(imagen1, imagen2):
     imagen1bw = cv.cvtColor(imagen1, cv.COLOR_BGR2GRAY)
     imagen2bw = cv.cvtColor(imagen2, cv.COLOR_BGR2GRAY)
+    imagen1bw = img_as_float(imagen1bw)
+    imagen2bw = img_as_float(imagen2bw)
     #Detección esquinas
     esquinasP = obtener_esquinas(imagen1bw)
     esquinasQ = obtener_esquinas(imagen2bw)
@@ -129,41 +130,40 @@ def ransac(imagen1, imagen2):
     puntosP = esquinasFiltradas[:,0]
     puntosQ = esquinasFiltradas[:,1]
 
-    tolerancia = 10**(-3) #Tolerancia con la que se considera que una Homografía es consistente para un par de puntos
-    minConsistentes = 0.8*puntosP.shape[0] #Mínima cantidad de esquinas que deben ser consistentes con una homografía
-    maxIter = 10**3 #Cantidad máxima de iteraciones
-
+    tolerancia = 2*10**(-1) #Tolerancia con la que se considera que una Homografía es consistente para un par de puntos
+    minConsistentes = 0.7*puntosP.shape[0] #Mínima cantidad de esquinas que deben ser consistentes con una homografía
+    maxIter = 10**5 #Cantidad máxima de iteraciones
     cantConsistentes = 0
     H = 0
     iter = 0
-
-    while(cantConsistentes > minConsistentes and iter < maxIter):
+    maxconsist = 0
+    while(cantConsistentes < minConsistentes and iter < maxIter):
         #Se seleccionan 4 puntos y se obtiene la homografía asociada
-        randInd = np.random.randint(puntosP.size, size = 4) 
+        randInd = np.random.randint(puntosP.shape[0], size = 4) 
         puntosPsel = puntosP[randInd]
         puntosQsel = puntosQ[randInd]
-
-        H = getHomography(puntosPsel, puntosQsel)
         
+        H = getHomography(puntosPsel, puntosQsel)
         #Se obtienen los puntos Q dados por P a traves de H
         puntosQEstimados = productHomography(H, puntosP)
 
         #Se calcula la cantidad de puntos considerados consistente con la tolerancia impuesta
-        cantConsistentes = np.sum(np.linalg.norm(puntosQEstimados - puntosQ, ord=2, axis=1) < tolerancia)
-
+        cantConsistentes = np.sum(np.linalg.norm(puntosQEstimados - puntosQ, ord=np.inf, axis=1) < tolerancia)
+        maxconsist = max(maxconsist, cantConsistentes/puntosQEstimados.shape[0])
         iter += 1
-    
+    print(maxconsist)
     #Puntos Q estimados con el último H computado
     puntosQEstimados = productHomography(H, puntosP)
     
     #Indices de puntos consistenes con el H
-    indConsistentes = np.nonzero(np.linalg.norm(puntosQEstimados - puntosQ, ord=2, axis=1) < tolerancia)
+    indConsistentes = np.nonzero(np.atleast_1d(np.linalg.norm(puntosQEstimados - puntosQ, ord=2, axis=1) < tolerancia))[0]
 
     #Se recuperan puntos consistentes con el último H computado
     puntosPConsistentes = puntosP[indConsistentes]
     puntosQConsistentes = puntosQ[indConsistentes]
 
     #Se computa el H que minimize B*h con la matriz B dada por puntos consistenes
+    print(indConsistentes, puntosPConsistentes.shape[0],iter)
     Hres = getHomography(puntosPConsistentes,puntosQConsistentes)
     
     return Hres
@@ -171,7 +171,7 @@ def ransac(imagen1, imagen2):
 def warping(imagen1, imagen2, H):
     #la imagen 2 se considerará la perspectiva de referencia
     esquinas = np.array([[0,0], [0,imagen1.shape[1]-1], [imagen1.shape[0]-1,0], [imagen1.shape[0]-1, imagen1.shape[1]-1]])
-    esquinasn = productHomography(H, esquinas)
+    esquinasn = np.int32(productHomography(H, esquinas))
     minfila = np.min(esquinasn[:,0])
     maxfila = np.max(esquinasn[:,0])
     mincol = np.min(esquinasn[:,1])
@@ -183,11 +183,17 @@ def warping(imagen1, imagen2, H):
     m = max(maxfila + offsetfila, imagen2.shape[0])
     n = max(maxcol + offsetcol, imagen2.shape[1])
 
+    im1w = np.zeros(maxfila+offsetfila, maxcol+offsetcol)
+    for i in range(imagen1.shape[0]):
+        for j in range(imagen1.shape[0]):
+            index = [i,j]
+            nindex = np.int32(simpleProductHomography(index)) + [offsetfila, offsetcol]
+            im1w[nindex[0],nindex[1]] = imagen1[i, j]
     
     or1 = np.array([0+offsetfila, 0+offsetcol])
     or2 = np.array([minfila+offsetfila, mincol+offsetcol])
     
-    return or1, or2, m, n 
+    return or1, or2, m, n, im1w
 
 
 #Blend simple
@@ -212,12 +218,15 @@ def filalpha(m,n):
 
 #Se asume que los valores de la imágenes van de 0 a 1
 def blend(imagen1, or1, imagen2, or2, m, n):
-    capaAlpha1 = filalpha(imagen1.shape[0],imagen1.shape[1])
-    capaAlpha2 = filalpha(imagen2.shape[0], imagen2.shape[1])
-    capas = imagen1.shape[2]
+    imagen1f = img_as_float(imagen1)
+    imagen2f = img_as_float(imagen2)
 
-    imagen1a = imagen1
-    imagen2a = imagen2
+    capaAlpha1 = filalpha(imagen1f.shape[0],imagen1f.shape[1])
+    capaAlpha2 = filalpha(imagen2f.shape[0], imagen2f.shape[1])
+    capas = imagen1f.shape[2]
+
+    imagen1a = imagen1f
+    imagen2a = imagen2f
     for i in range(capas):
         imagen1a[:,:,i] = imagen1a[:,:,i] * capaAlpha1  
         imagen2a[:,:,i] = imagen2a[:,:,i] * capaAlpha2  
@@ -266,15 +275,15 @@ def dice(imagen1, imagen2):
 
 def main():
     imagen1 = cv.imread("./der.png", cv.IMREAD_COLOR)
-    imagen1 = img_as_float(imagen1)
+
     imagen2 = cv.imread("./izq.png", cv.IMREAD_COLOR)
-    imagen2 = img_as_float(imagen2)
+
 
     H = ransac(imagen1, imagen2)
 
-    or1, or2, m, n = warping(imagen1, imagen2, H)
+    or1, or2, m, n, imw1 = warping(imagen1, imagen2, H)
 
-    imagenRes = blend(imagen1, or1, imagen2, or2, m, n)
+    imagenRes = blend(imw1, or1, imagen2, or2, m, n)
 
     fig, ax = plt.subplots(1,3, shape=(10,10))
     ax[0].imshow(imagen1)
