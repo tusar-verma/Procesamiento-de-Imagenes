d77a23caadb9fd5aabd53c05d599e956bd0f88df
Victor <victorolivarez2001@hotmail.com>
2024-12-07 16:35:07 -0300

    thresholding

--- TrabajoFinal/dice.py
+++ TrabajoFinal/dice.py
@@ -56,4 +56,55 @@ def dice_promedio():
     coef_dice = coef_dice / i
     return coef_dice
 
-print(dice_promedio())
+
+def mati_thresh(i1,i2):
+    m1 = i1.shape[0]
+    n1 = i1.shape[1]
+    m2 = i2.shape[0]
+    n2 = i2.shape[1]
+    thresh = 15
+    M = max(m1,m2)
+    N = max(n1,n2)
+    
+    im1padd = -np.ones((M,N,3))
+    im2padd = -(1+thresh)*np.ones((M,N,3))
+
+    im1padd[:m1,:n1,:] = i1
+    im2padd[:m2,:n2,:] = i2
+    
+    bm =((np.abs(im1padd[:,:,0] - im2padd[:,:,0] )<thresh)& (np.abs(im1padd[:,:,1] -im2padd[:,:,1])<thresh) & (np.abs(im1padd[:,:,2] - im2padd[:,:,2]) < thresh))
+    dice_coeff = np.sum(bm)/(im1padd.shape[0]*im2padd.shape[1])
+    return dice_coeff
+
+def thresholding():
+    i = 0
+    coef_dice = 0
+    for imagen in os.listdir("imagenes/Forest"):
+        
+        #leeo la imagen
+        if(i < 61 ): #1999*0.03
+            imagen_original = cv.imread(f"imagenes/Forest/{imagen}", cv.IMREAD_COLOR)
+            mitad = int(imagen_original.shape[1] * (1/2)) +17
+            porcion = random.randint(mitad, imagen_original.shape[1])
+
+            #dos_tercios = int(imagen_original.shape[1] * porcion)
+            dos_tercios = porcion
+            un_tercio   = imagen_original.shape[1] - dos_tercios
+            print(dos_tercios, un_tercio)
+            imagen_1 = imagen_original[0:imagen_original.shape[0],0:dos_tercios+1]
+            imagen_2 = imagen_original[0:imagen_original.shape[0],un_tercio:imagen_original.shape[1]]
+            print(imagen_1.shape,imagen_2.shape,imagen_original.shape)
+            imagen_generada = mo.mosaico(imagen_1,imagen_2)
+            imagen_generada = img_as_ubyte(imagen_generada)
+            cv.imwrite(f"resultados_dice/{i}.png",imagen_generada)
+            
+            dice = mati_dice(imagen_generada,imagen_original)
+            print(dice)
+            coef_dice += dice
+            i+=1
+            
+
+    coef_dice = coef_dice / i
+    return coef_dice
+
+print(thresholding())
--- TrabajoFinal/panoramicas.py
+++ TrabajoFinal/panoramicas.py
@@ -22,9 +22,39 @@ def dana():
             imagen_2 = cv.imread(f"imagenes/dana/{pan}/{lista_partes[i]}", cv.IMREAD_COLOR)
             imagen_generada = img_as_ubyte(mo.mosaico(imagen_2,imagen_generada))
             cv.imwrite(f"resultados_dana/{pan}_iteracion{i}.png",imagen_generada)
+            #plt.imshow(imagen_generada)
+            #plt.show()
+        
+        
+        cv.imwrite(f"resultados_dana/{pan}.png",imagen_generada)
+
+def dana_centrado():
+    iter = 0
+    img_pan = os.listdir("imagenes/dana")
+    img_pan.sort()
+    for pan in img_pan:
+        lista_partes = os.listdir(f"imagenes/dana/{pan}")
+        lista_partes.sort()
+        print(lista_partes)
+        imagen_generada = 0 
+        medio = len(lista_partes)//2
+        for i in range(medio, len(lista_partes)):
+            if(i==medio):
+                imagen_generada = cv.imread(f"imagenes/dana/{pan}/{lista_partes[i]}", cv.IMREAD_COLOR)
+                continue
+            imagen_2 = cv.imread(f"imagenes/dana/{pan}/{lista_partes[i]}", cv.IMREAD_COLOR)
+            imagen_generada = img_as_ubyte(mo.mosaico(imagen_2,imagen_generada))
+            cv.imwrite(f"resultados_dana/{pan}_iteracion{iter}.png",imagen_generada)
             plt.imshow(imagen_generada)
             plt.show()
-        
+            iter += 1
+        for i in range(medio):
+            imagen_2 = cv.imread(f"imagenes/dana/{pan}/{lista_partes[medio-1-i]}", cv.IMREAD_COLOR)
+            imagen_generada = img_as_ubyte(mo.mosaico(imagen_2,imagen_generada))
+            cv.imwrite(f"resultados_dana/{pan}_iteracion{iter}.png",imagen_generada)
+            #plt.imshow(imagen_generada)
+            #plt.show()
+            iter+=1
         
         cv.imwrite(f"resultados_dana/{pan}.png",imagen_generada)
 dana()
